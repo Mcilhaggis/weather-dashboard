9b8be57beff641dbec4895a2a2608fd0b4f0f96c
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-11-09 16:50:54 -0500

    adding buttons to previously searched bar

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 //Global variables
 var cityName;
 var mainCity = document.querySelector(".city-name");
@@ -8,7 +7,6 @@ var mainHumidity = document.querySelector(".humidity");
 var mainWindSpeed = document.querySelector(".wind-speed");
 var mainUvIndex = document.querySelector(".uv-index");
 var uvBox = document.querySelector(".uv-box");
-
 //Time Variables
 var today = moment().format("l");
 var m = moment();
@@ -23,7 +21,6 @@ $('.btn').click(function(e){
 
     //add function here for adding to previously searched list and maybe a second one for storing the data 
 
-
 }); 
 
 //Getting the one-day forecast for searched city 
@@ -35,7 +32,6 @@ fetch(weatherAPI)
     return response.json();
 })
 .then((data) => {
-    console.log(data);
     //icon link needs fixed
             mainCity.innerText = data.name + ' ' + today + ' '
             $(".current-icon").attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
@@ -73,12 +69,10 @@ fetch(weatherAPI)
 
 //Getting the 5-day forecast for the immediately searched city 
 function getFiveDayAPI(city){
-    console.log(city);
     var weatherFiveDayAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=6b0d5c546226a11c17010c1077322954';
     
     fetch(weatherFiveDayAPI)
     .then(function(response){
-        console.log(response);
         return response.json();
         
     })
@@ -88,71 +82,88 @@ function getFiveDayAPI(city){
 
         //5-day card variables 
 var fiveDayCards = document.querySelectorAll(".five-card");
-var fiveDayIcon = document.querySelectorAll(".five-icon");
-var fiveDayDate = document.querySelector(".date-five");
-var fiveDayTemp = document.querySelector(".temp-five");
-var fiveDayHumidity = document.querySelector(".humid-one");
 
+var cardIndex = 0;
         //adding dates to each of the cards from today's day onwards 
         for (i = 0; i < results.length; i+= 8){
-
-                //adding date to five day forecast
-                fiveDayDate.innerText = m.add(1, 'day').format("l");
-
-                //adding weather icon to five day forecast
+                var currentCard = fiveDayCards[cardIndex];
+                //Set the date
+                var currentDayDate = currentCard.querySelector(".date-five");
+                currentDayDate.innerText = m.add(1, 'day').format("l");
+                //Set the icon
+                var currentDayIcon = currentCard.querySelector(".five-icon");
                 var weatherIcon = data.list[i].weather[0].icon;
-                console.log(weatherIcon);
-                $(".five-icon").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png");
-
-                //add the temp to the cards
-                fiveDayTemp.innerText = 'Temp: ' + data.list[i].main.temp + ' °F';
-                console.log(fiveDayTemp.innerText);
-
-                //adding humidity to each card
-                fiveDayHumidity.innerText = 'Humidity: ' + data.list[i].main.humidity + '%';
-                console.log(fiveDayHumidity.innerText);
-
+                currentDayIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png")
+                //Set the Temp   
+                var currentDayTemp = currentCard.querySelector(".temp-five");
+                currentDayTemp.innerText = 'Temp: ' + data.list[i].main.temp + ' °F';
+                //Set the humidity
+                var currentDayHumidity = currentCard.querySelector(".humid-five");
+                currentDayHumidity.innerText = 'Humidity: ' + data.list[i].main.humidity + '%';
+                cardIndex++;
             }
-                })
-
-                }
+        
+    })
+}
 
     
 //Create Function for Saving user input from search bar
 
 $('#save-search').on('click', function(){
-    $('input[type="text"]').each(function(){
+    $('input[type="text"]').each(function(){        
         var searchedCity = $(this).val();
-        //probably want to store the details as an array for resuse
-        var weatherDetails = $(this).siblings("input").val();
-        var storedArr= [];
-        storedArr.push(weatherDetails);
-        localStorage.setItem(searchedCity, JSON.stringify(storedArr));
+        var previouslySearchedArr = JSON.parse(localStorage.getItem("citiesArr")) || [];
+        previouslySearchedArr.push(searchedCity);
+        localStorage.setItem("citiesArr", JSON.stringify(previouslySearchedArr));
 
 
-//add splice() to limit the number of search results shown 
         // getAPI(searchedCity);
         // getFiveDayAPI(searchedCity);
     });
 });
 
-//append the city name to the list under the seach bar
 
-//Calls stored items on page load
-function pageLoad () {
-    var lastSearch = JSON.parse(localStorage.getItem('searchedCity'));
-    var searchDiv = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded search-history' style='width: 12rem;'>").text(lastSearch);
-    var psearch = $(".previouslySearched");
-    psearch.append(searchDiv)
-    $("#search-history").prepend(psearch);
-}
+// //Calls stored items on page load
+// function pageLoad () {
+//     var lastSearch = JSON.parse(localStorage.getItem('searchedCity'));
+    // var searchDiv = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded search-history' style='width: 12rem;'>").text(lastSearch);
+//     var psearch = $(".previouslySearched");
+//     psearch.append(searchDiv)
+//     $("#search-history").prepend(psearch);
+// }
 
-pageLoad();
+// pageLoad();
 
-//Event deligation...
-$("#search-history").on('click', '.btn', function(event) {
-event.preventDefault();
-    console.log($(this).text());
-    searchedCity($(this).text());
+// //Event deligation...
+// $("#search-history").on('click', '.btn', function(event) {
+// event.preventDefault();
+//     console.log($(this).text());
+//     searchedCity($(this).text());
 
-});
+// });
+
+// //Function for creating the buttons 
+function searchButtons() {
+    //check local storage to check for key (past searches)
+    var buttonValueArray = JSON.parse(localStorage.getItem("citiesArr"));
+    console.log(buttonValueArray);
+    //loop over the values in the array (the city names)
+    for (i = 0; i < buttonValueArray.length; i++){
+        $(".previouslySearched").append($("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded search-history' style='width: 100%;'>").text(buttonValueArray[i]));
+        //Add event listener on the button will run the search function 
+    }
+    var value = $(".search-history").text();
+    console.log(value);
+
+        $(".search-history").click(function(e){
+            e.preventDefault();
+
+            // getAPI(button text value );
+            // getFiveDayAPI(button text value);
+        });
+    
+    }
+   
+
+searchButtons();
+//Append the button onto the page 
